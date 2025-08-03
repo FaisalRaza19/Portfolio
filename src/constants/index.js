c0d00f48c8cacd9914d9313ca6ddf2d1bd803bbf
Faisal Raza <faisalrazaqazi2000@gmail.com>
2025-08-03 18:22:44 +0500

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,222 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "Job Portal",
+    description:
+      "Job Portal Web Application Full-Stack MERN Application.",
+    subDescription: [
+      "A modern job portal built with the MERN stack where job seekers can easily find and apply for jobs, upload resumes, and chat with employers in real time. Employers can post jobs, manage applicants, and track application status.",
+      "It includes role-based login, smart filtering, resume preview, and AI-powered skill suggestions.",
+      "Users can leave reviews for companies, and both sides get email updates on application status.",
+      "Everything's built with a clean, responsive UI and smooth UX."
+    ],
+    href: "https://github.com/FaisalRaza19/Mern_Job_Portal",
+    logo: "",
+    image: "public/assets/projects/web1.png",
+    tags: [
+      {
+        id: 1,
+        name: "vite",
+        path: "public/assets/logos/vitejs.svg",
+      },
+      {
+        id: 2,
+        name: "JS",
+        path: "public/assets/logos/js.png",
+      },
+      {
+        id: 3,
+        name: "Node Js",
+        path: "public/assets/logos/Node.js.svg",
+      },
+      {
+        id: 4,
+        name: "Express Js",
+        path: "public/assets/logos/Express.svg",
+      },
+      {
+        id: 5,
+        name: "NPM",
+        path: "public/assets/logos/NPM.svg",
+      },
+      {
+        id: 6,
+        name: "Mongoose",
+        path: "public/assets/logos/Mongoose.js.svg",
+      },
+      {
+        id: 6,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "E-Commerec Multi Vendor",
+    description:
+      "This is a fully functional multi-vendor e-commerce web application I built using the MERN stack (MongoDB, Express.js, React.js, Node.js).",
+    subDescription: [
+      "This is a multi-vendor e-commerce web app built with the MERN stack.",
+      "It lets different sellers create shops, add products, and manage orders easily.",
+      "Buyers can browse, save favorites, and check out smoothly.",
+      "The app also includes real-time updates and a clean, user-friendly design for a seamless shopping experience."
+    ],
+    href: "https://github.com/FaisalRaza19/Multi_Vendor",
+    logo: "",
+    image: "public/assets/projects/web2.png",
+    tags: [
+      {
+        id: 1,
+        name: "vite",
+        path: "public/assets/logos/vitejs.svg",
+      },
+      {
+        id: 2,
+        name: "JS",
+        path: "public/assets/logos/js.png",
+      },
+      {
+        id: 3,
+        name: "Node Js",
+        path: "public/assets/logos/Node.js.svg",
+      },
+      {
+        id: 4,
+        name: "Express Js",
+        path: "public/assets/logos/Express.svg",
+      },
+      {
+        id: 5,
+        name: "NPM",
+        path: "public/assets/logos/NPM.svg",
+      },
+      {
+        id: 6,
+        name: "Mongoose",
+        path: "public/assets/logos/Mongoose.js.svg",
+      },
+      {
+        id: 6,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Gemini Clone",
+    description:
+      "This is a fully functional Gemini Clone web application I built using the MERN stack (MongoDB, Express.js, React.js, Node.js).",
+    subDescription: [
+      "This is a multi-vendor e-commerce web app built with the MERN stack.",
+      "On our platform, users can easily register, log in, and log out.",
+      "while having the flexibility to update their profile avatars.",
+      "Our interactive chat system allows users to ask questions just like Gemini.",
+      "where each conversation is assigned a unique Chat ID for easy reference."
+    ],
+    href: "https://github.com/FaisalRaza19/Full_Stack_Gemini_Clone",
+    logo: "",
+    image: "public/assets/projects/web3.png",
+    tags: [
+      {
+        id: 1,
+        name: "vite",
+        path: "public/assets/logos/vitejs.svg",
+      },
+      {
+        id: 2,
+        name: "JS",
+        path: "public/assets/logos/js.png",
+      },
+      {
+        id: 3,
+        name: "Node Js",
+        path: "public/assets/logos/Node.js.svg",
+      },
+      {
+        id: 4,
+        name: "Express Js",
+        path: "public/assets/logos/Express.svg",
+      },
+      {
+        id: 5,
+        name: "NPM",
+        path: "public/assets/logos/NPM.svg",
+      },
+      {
+        id: 6,
+        name: "Mongoose",
+        path: "public/assets/logos/Mongoose.js.svg",
+      },
+      {
+        id: 6,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "News Monkey",
+    description:
+      "This is a fully functional News Monkey Web App.",
+    subDescription: [
+      "This is a News Monkey web app built with the React Js.",
+      "Our platform user can read the news on current affairs",
+      "User can read news by category",
+      "User can see more deatils of particular news"
+    ],
+    href: "https://github.com/FaisalRaza19/News-Monkey",
+    logo: "",
+    image: "public/assets/projects/web4.png",
+    tags: [
+      {
+        id: 1,
+        name: "vite",
+        path: "public/assets/logos/vitejs.svg",
+      },
+      {
+        id: 2,
+        name: "JS",
+        path: "public/assets/logos/js.png",
+      },
+      {
+        id: 6,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+];
+
+export const mySocials = [
+  {
+    name: "WhatsApp",
+    href: "https://wa.me/92252518398",
+    icon: "/assets/socials/whatsApp.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "www.linkedin.com/in/heyfaisalraza",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/faisal_raza_2007",
+    icon: "/assets/socials/instagram.svg",
+  },
+];
+
+export const experiences = [
+  {
+    title: "MERN Stack Developer",
+    job: "Self-Initiated Projects",
+    date: "2023-2025",
+    contents: [
+      "I’ve been fully focused on learning and building full-stack web applications using the MERN stack.",
+      "I’ve completed multiple real-world projects, including a multi-vendor e-commerce platform and a professional job portal with real-time chat, payments, and more.",
+      "Now actively looking for a role where I can contribute, grow, and build amazing things with a great team."
+    ],
+  },
+];
